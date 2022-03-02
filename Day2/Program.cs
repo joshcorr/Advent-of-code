156cec59923d7a7ce4bc123d856ba55c8f7fb569
Josh Corrick <joshcorr@users.noreply.github.com>
2022-03-01 23:14:29 -0500

    Day2. Puzzle 1.

---
+++ Day2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Puzzle 1
+            String fileName = @"..\..\..\Data.txt";
+            //String fileName = @"..\..\..\Test.txt";
+            var content = File.ReadAllLines(fileName);
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < content.Length; i++)
+            {
+                string[] movement = content[i].Split(' ');
+                //Console.WriteLine($"Going to do {movement[0]} in {movement[1]}");
+                switch (movement[0].ToLower())
+                {
+                    case "forward" :
+                        x += int.Parse(movement[1]);
+                        break;
+                    case "down" :
+                        y += int.Parse(movement[1]);
+                        break;
+                    case "up" :
+                        y -= int.Parse(movement[1]);
+                        break;
+                    default :
+                        Console.WriteLine($"Unsure what to do with {movement[0]}");
+                        break;
+                }
+            }
+            int total = x * y;
+            Console.WriteLine($"Submarine is at postion X:{x}, Y:{y} for a combined {total}");
+
+        }
+    }
+}
